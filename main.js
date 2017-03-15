ae3a70662cdaff66d87e77305077cbbe612f9def
John Carter <johngallweycarter@gmail.com>
2017-03-15 01:16:17 -0400

    finally got the fucking places showing

--- main.js
+++ main.js
@@ -7,6 +7,58 @@ var jsonfile = require('jsonfile')
 
 var json_file = './places.json'
 
+var in_file = json_file
+
+var out_file = './places_final.json'
+
+
+function extractJsonInfo() {
+  var inJson;
+  var final_obj
+
+
+  jsonfile.readFileSync(in_file, function(err, obj) {
+    var obj_array = obj.final
+    var location_obj = {}
+
+    for (var i = 0; i < obj_array.length; i++) {
+      var obj = obj_array[i];
+      var location = obj.results[0].geometry.location
+      location_obj.location = location
+    }
+
+    final_obj = location_obj
+  })
+  return final_obj
+}
+
+
+function extractInfo() {
+  var inJson
+  var location_obj = {}
+  location_obj.locations = []
+
+  var data = jsonfile.readFileSync(in_file)
+
+  obj_array = data.final
+
+  for (var i = 0; i < obj_array.length; i++) {
+    var obj = obj_array[i];
+    var location = obj.results[0].geometry.location
+    location_obj.locations.push(location)
+  }
+
+  return location_obj
+}
+
+
+function writeExtractedInfo(){
+	info = extractInfo();
+	jsonfile.writeFileSync(out_file, info);
+	console.log("sex")
+}
+
+writeExtractedInfo();
 
 // function getGmaps(query, callback) {
 //   return http.get('http://maps.googleapis.com/maps/api/geocode/json?address=' + query + '&sensor=false', function(response) {
@@ -22,7 +74,7 @@ var json_file = './places.json'
 // }
 
 function getGmaps(loc, callback) {
-	loc += " ann arbor, michigan"
+  loc += " ann arbor, michigan"
   var query = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + loc + '&sensor=false';
   var res = request('GET', query)
   var body = res.body.toString()
@@ -31,17 +83,17 @@ function getGmaps(loc, callback) {
 }
 
 
-(function() {
-  obj = {};
-  obj.final = []
-  map.locations.forEach(function(location) {
-    getGmaps(location, function(json) {
-    	console.log("getting info for " + location);
-      obj.final.push(json)
-    })
-  })
-  jsonfile.writeFileSync(json_file, obj)
-})()
+// (function() {
+//   obj = {};
+//   obj.final = []
+//   map.locations.forEach(function(location) {
+//     getGmaps(location, function(json) {
+//     	console.log("getting info for " + location);
+//       obj.final.push(json)
+//     })
+//   })
+//   jsonfile.writeFileSync(json_file, obj)
+// })()
 
 
 
